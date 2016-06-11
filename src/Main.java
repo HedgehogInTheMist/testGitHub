f2cd1e4655aee3ba0cf0adab2ca02fdc20b48830
Hedgehog <edovin.sergey@gmail.com>
2016-06-11 05:02:45 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there! It's a test of github + intellij idea ");
         //second commit
+        //third commit
     }
 }
